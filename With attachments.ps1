c20b70a0054b502f9f7f8fa72eb621c4826d9f10
ARTPON <guo2006@yandex.ru>
2019-02-12 14:34:54 +0400

    Minor updates

--- With attachments.ps1
+++ With attachments.ps1
@@ -1,27 +1,25 @@
-﻿
 
-
-#Variable for System Count
+#Сбрасываем служебные переменные
 $x = 0
 $emsg = ""
 
-#Report Date
+#Дата отчета
 $rptDate = Get-Date
 
-#Device List Location
-$Devices = "C:\Users\aiponevin\Documents\keePass\KeePass-1.36\Devices.txt"
+#Список адресов для пинга
+$Devices = "C:\ListAdresses.txt"
 
-#Load Array of Computer Systems
+#Получаем адреса для пинга в переменную
 $networkSystems = Get-Content $Devices
 
-#Ping Each System in Array
+#Пингуем каждый адрес из списка
 foreach($ns in $networkSystems)
 {
-#If Ping Fails then Add to Message
+#Если пинг неудачен - добавляем адрес в отчет
 if (!(test-connection -computername $ns -quiet))
 {
 $x = $x + 1
-$emsg = $emsg + $ns + "<br />"
+$emsg = $ns + "<br />"
 }
 
 }
@@ -29,26 +27,28 @@ $emsg = $emsg + $ns + "<br />"
 if($x -gt 0)
 
 {
-$serverSmtp = "smtp.yandex.ru" 
+
+#Адрес сервера SMTP для отправки
+$serverSmtp = "smtp.yandex.ru"
 
 #Порт сервера
 $port = 587
 
 #От кого
-$From = "login@yandex.ru" 
+$From = "login@yandex.ru"
 
 #Кому
-$To = "login@yandex.ru" 
+$To = "somemail@yandex.ru"
 
 #Тема письма
-$subject = "Alert, smth is not working"
+$subject = "Alert, smth is not working    " + $rptDate
 
 #Логин и пароль от ящики с которого отправляете login@yandex.ru
 $user = "login"
-$pass = "password"
+$pass = "*****"
 
-#Путь до файла 
-$file = "C:\File.txt"
+#Путь до файла
+$file = "C:\FileYouWantToSend.txt"
 
 #Создаем два экземпляра класса
 $att = New-object Net.Mail.Attachment($file)
@@ -56,25 +56,24 @@ $mes = New-Object System.Net.Mail.MailMessage
 
 #Формируем данные для отправки
 $mes.From = $from
-$mes.To.Add($to) 
-$mes.Subject = $subject 
-$mes.IsBodyHTML = $true 
-$mes.Body = "<h1>Тестовое письмо</h1>" + $emsg
+$mes.To.Add($to)
+$mes.Subject = $subject
+$mes.IsBodyHTML = $true
+$mes.Body = "<h1>Тестовое письмо c  вложением</h1>"
 
 #Добавляем файл
-$mes.Attachments.Add($att) 
+$mes.Attachments.Add($att)
 
-#Создаем экземпляр класса подключения к SMTP серверу 
+#Создаем экземпляр класса подключения к SMTP серверу
 $smtp = New-Object Net.Mail.SmtpClient($serverSmtp, $port)
 
-#Сервер использует SSL 
-$smtp.EnableSSL = $true 
+#Сервер использует SSL
+$smtp.EnableSSL = $true
 
 #Создаем экземпляр класса для авторизации на сервере яндекса
 $smtp.Credentials = New-Object System.Net.NetworkCredential($user, $pass);
 
 #Отправляем письмо, освобождаем память
-$smtp.Send($mes) 
+$smtp.Send($mes)
 $att.Dispose()
-
 }
--- Without_attachments.ps1
+++ Without_attachments.ps1
@@ -19,7 +19,7 @@ foreach($ns in $networkSystems)
 if (!(test-connection -computername $ns -quiet))
 {
 $x = $x + 1
-$emsg = $emsg + $ns + "<br />"
+$emsg = $ns + "<br />"
 }
 
 }
@@ -27,24 +27,23 @@ $emsg = $emsg + $ns + "<br />"
 if($x -gt 0)
 
 {
-$serverSmtp = "smtp.yandex.ru" 
+$serverSmtp = "smtp.yandex.ru"
 
 #Порт сервера
 $port = 587
 
 #От кого
-$From = "login@yandex.ru" 
+$From = "login@yandex.ru"
 
 #Кому
-$To = "login@yandex.ru" 
+$To = "login@yandex.ru"
 
 #Тема письма
-$subject = "Alert, smth is not working"
+$subject = "Alert, smth is not working    " + $rptDate
 
 #Логин и пароль от ящики с которого отправляете login@yandex.ru
 $user = "login"
 $pass = "*****"
-
 
 #Создаем экземпляр класса
 
@@ -52,22 +51,20 @@ $mes = New-Object System.Net.Mail.MailMessage
 
 #Формируем данные для отправки
 $mes.From = $from
-$mes.To.Add($to) 
-$mes.Subject = $subject 
-$mes.IsBodyHTML = $true 
+$mes.To.Add($to)
+$mes.Subject = $subject
+$mes.IsBodyHTML = $true
 $mes.Body = "<h1>Тестовое письмо</h1>" + $emsg
 
-
-#Создаем экземпляр класса подключения к SMTP серверу 
+#Создаем экземпляр класса подключения к SMTP серверу
 $smtp = New-Object Net.Mail.SmtpClient($serverSmtp, $port)
 
-#Сервер использует SSL 
-$smtp.EnableSSL = $true 
+#Сервер использует SSL
+$smtp.EnableSSL = $true
 
 #Создаем экземпляр класса для авторизации на сервере яндекса
 $smtp.Credentials = New-Object System.Net.NetworkCredential($user, $pass);
 
 #Отправляем письмо
 $smtp.Send($mes) 
-
 }
